56d04b5ffe7eee86772a22449760d4723c5db56a
Thomas Gardner <tpgardner19@gmail.com>
2017-07-14 06:49:34 -0500

    messing with onkeyup event listener

--- javascript/cli.js
+++ javascript/cli.js
@@ -1,3 +1,19 @@
 /*
     This script contains functions for emulating terminal behavior on this website
 */
+function changeDirectory(destination) {
+    // mimic the 'cd' command
+}
+
+function listDirectory(destination) {
+    // mimic the 'ls' command
+}
+
+function showManual(command) {
+    // mimic the 'man' command
+}
+
+document.onkeyup = function(evt) {
+    var eventObj = window.event? event : evt
+    console.log(JSON.stringify(eventObj))
+}
